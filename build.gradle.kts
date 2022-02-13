4b146fd6c9fb301385c570d97d51a95203b0db50
Ito-N <theme4087abcdef@gmail.com>
2022-02-13 17:31:39 +0900

    ローカル環境構築

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val hikaricp_version: String by project
+val postgres_version: String by project
+val flyway_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-locations:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-freemarker:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("org.flywaydb:flyway-core:$flyway_version")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/example/factory/DatabaseFactory.kt
@@ -0,0 +1,38 @@
+package com.example.factory
+
+import com.typesafe.config.*
+import com.zaxxer.hikari.*
+import io.ktor.config.*
+import org.flywaydb.core.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.*
+
+object DatabaseFactory {
+    private val appConfig = HoconApplicationConfig(ConfigFactory.load())
+    private val dbUrl = appConfig.property("db.jdbcUrl").getString()
+    private val dbUser = appConfig.property("db.dbUser").getString()
+    private val dbPassword = appConfig.property("db.dbPassword").getString()
+
+    fun init() {
+        Database.connect(hikari())
+        val flyway = Flyway.configure().dataSource(dbUrl, dbUser, dbPassword).load()
+        flyway.migrate()
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = "org.postgresql.Driver"
+        config.jdbcUrl = dbUrl
+        config.username = dbUser
+        config.password = dbPassword
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
+
+    suspend fun <T> dbQuery(
+        block: suspend () -> T): T =
+        newSuspendedTransaction { block() }
+}
